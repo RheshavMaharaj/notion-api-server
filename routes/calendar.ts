177e10e19020be3c4c8f3625b3d39c13c1c68d7d
RheshavMaharaj <rheshav@hotmail.com>
2022-03-21 19:44:24 +1100

    Added endpoints for local testing and setup notion connection

--- routes/calendar.ts
+++ routes/calendar.ts
@@ -1,18 +1,87 @@
 import express from 'express';
-// import notion from '../modules/notion';
+import notion from '../modules/notion';
 
 const calendarRoutes = express.Router();
 
-calendarRoutes.route('/new-event').post(function(req, res) {
-  console.log(req.body);
+declare let process: {
+  env: {
+    NOTION_CALENDAR_ID: string
+  }
+};
+
+const databaseId = process.env.NOTION_CALENDAR_ID;
+
+calendarRoutes.route('/calendar/new-event').post(async function(req, res) {
+  const { title, date, notes, location } = req.body;
+
+  const response = await notion.pages.create({
+    parent: { database_id: databaseId },
+    properties: {
+      title: {
+        title: [
+          {
+            text: {
+              content: title,
+            },
+          },
+        ],
+      },
+      Date: {
+        type: 'date',
+        date: {
+          start: date.start,
+          end: date.end,
+        },
+      },
+      Notes: {
+        type: 'rich_text',
+        rich_text: [
+          {
+            text: {
+              content: notes,
+            },
+          },
+        ],
+      },
+      Location: {
+        files: [
+          {
+            name: location.name,
+            type: 'external',
+            external: {
+              url: location.url,
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  if (response.id) {
+    res.json(response);
+  }
+  else {
+    res.sendStatus(400);
+  }
 });
 
-calendarRoutes.route('/delete-event').post(function(req, res) {
-  console.log('hello');
-});
+calendarRoutes.route('/calendar/delete-event').post(async function(req, res) {
+  const queryResponse = await notion.databases.query({
+    database_id: databaseId,
+  });
 
-calendarRoutes.route('/update-event').post(function(req, res) {
-  console.log('hello');
+  // @ts-expect-error
+  const targetPage = queryResponse.results.find(page => page.properties.Name.title[0].text.content === req.body.name);
+
+  if (targetPage) {
+    const response = await notion.blocks.delete({
+      block_id: targetPage.id,
+    });
+    res.json(response);
+  }
+  else {
+    res.json({ message: 'invalid request' });
+  }
 });
 
 module.exports = calendarRoutes;
