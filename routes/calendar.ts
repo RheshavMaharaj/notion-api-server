b49c66f4ebdbc0773acad69c79f92c10bd171feb
RheshavMaharaj <rheshav@hotmail.com>
2022-03-22 17:59:56 +1100

    Added api endpoint for creating all day events

--- routes/calendar.ts
+++ routes/calendar.ts
@@ -11,7 +11,7 @@ declare let process: {
 
 const databaseId = process.env.NOTION_CALENDAR_ID;
 
-calendarRoutes.route('/calendar/new-event').post(async function(req, res) {
+calendarRoutes.route('/calendar/create/event').post(async function(req, res) {
   const { title, date, notes, location } = req.body;
 
   const response = await notion.pages.create({
@@ -65,13 +65,69 @@ calendarRoutes.route('/calendar/new-event').post(async function(req, res) {
   }
 });
 
+calendarRoutes.route('/calendar/create/all-day').post(async function(req, res) {
+  const { title, date, notes, location } = req.body;
+
+  const response = await notion.pages.create({
+    parent: { database_id: databaseId },
+    properties: {
+      title: {
+        title: [
+          {
+            text: {
+              content: title,
+            },
+          },
+        ],
+      },
+      Date: {
+        type: 'date',
+        date: {
+          start: date.start,
+        },
+      },
+      Notes: {
+        type: 'rich_text',
+        rich_text: [
+          {
+            text: {
+              content: notes,
+            },
+          },
+        ],
+      },
+      Location: {
+        files: [
+          {
+            name: location.name,
+            type: 'external',
+            external: {
+              url: location.url,
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  if (response.id) {
+    res.json(response);
+  }
+  else {
+    res.sendStatus(400);
+  }
+});
+
 calendarRoutes.route('/calendar/delete-event').post(async function(req, res) {
   const queryResponse = await notion.databases.query({
     database_id: databaseId,
   });
 
-  // @ts-expect-error
-  const targetPage = queryResponse.results.find(page => page.properties.Name.title[0].text.content === req.body.name);
+  const targetPage = queryResponse.results.find(page =>
+    // @ts-expect-error
+    page.properties.Name.title[0].text.content === req.body.name &&
+    // @ts-expect-error
+    page.properties.Date.date.start === req.body.date);
 
   if (targetPage) {
     const response = await notion.blocks.delete({
