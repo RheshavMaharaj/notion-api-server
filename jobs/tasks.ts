636bfdd10f7c9a581f1d3b08e28bbba90f412931
RheshavMaharaj <rheshav@hotmail.com>
2022-03-28 19:52:19 +1100

    Added tasks automation, Won't fix: Streaks

---
+++ jobs/tasks.ts
@@ -0,0 +1,55 @@
+import notion from '../modules/notion';
+import moment, { Moment } from 'moment';
+import 'moment-timezone';
+
+declare let process: {
+  env: {
+    NOTION_TASKS_DATABASE_ID: string,
+    NOTION_STREAK_PAGE_ID: string,
+  }
+};
+
+function createNewDate(date: Date): Moment {
+  return moment(date).add(1, 'days').tz('Australia/Sydney');
+}
+
+async function tasksJob() {
+  if (moment().weekday() >= 6) return;
+  const databaseId = process.env.NOTION_TASKS_DATABASE_ID;
+  const today = moment().startOf('day');
+
+  const queryResponse = await notion.databases.query({
+    database_id: databaseId,
+    page_size: 100,
+    filter: {
+      or: [
+        {
+          property: 'Date',
+          date: {
+            before: today.toISOString(),
+          },
+        },
+      ],
+    },
+  });
+
+  const relevantResults = queryResponse.results;
+
+  for await (const result of relevantResults) {
+    await notion.pages.update({
+      page_id: result.id,
+      properties: {
+        Date: {
+          date: {
+            // @ts-expect-error
+            start: createNewDate(result.properties.Date.date.start).toISOString(),
+            // @ts-expect-error
+            end: createNewDate(result.properties.Date.date.end).toISOString(),
+          },
+        },
+      },
+    });
+  }
+}
+
+export default tasksJob;
