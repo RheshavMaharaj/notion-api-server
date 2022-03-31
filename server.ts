15626bdc555bee9b4a2fc6b3f02afb14ee4c9771
RheshavMaharaj <rheshav@hotmail.com>
2022-04-01 09:33:52 +1100

    Added ejs for widget templating and configured the server to use ejs view engine

--- server.ts
+++ server.ts
@@ -5,11 +5,15 @@ import bookJob from './jobs/books';
 import { tasksJob, archiveJob } from './jobs/tasks';
 const app = express();
 
+app.set('view engine', 'ejs');
+
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(require('./routes/calendar'));
+app.use(require('./routes/reminders'));
+app.use(require('./routes/widgets'));
 
 const minutes = 10; // TODO RM: In Testing, using a smaller interval
 const interval = minutes * 1000; // TODO RM: Add minute modifier
